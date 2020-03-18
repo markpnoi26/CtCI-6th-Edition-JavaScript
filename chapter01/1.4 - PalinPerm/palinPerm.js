fbd2a084e19dc36ec1a169ed3fa0255ce38214b2
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-03-18 08:59:24 -0700

    added working solution to palinPerm

--- chapter01/1.4 - PalinPerm/palinPerm.js
+++ chapter01/1.4 - PalinPerm/palinPerm.js
@@ -1,7 +1,30 @@
 var palinPerm = function(string) {
-  
+    let newString = string.toLowerCase()
+    let obj = {}
+    for (let i=0; i < newString.length; i++) {
+        if (newString[i] !== " ") {
+            if (obj[newString[i]]) {
+                obj[newString[i]]++
+            } else {
+                obj[newString[i]] = 1
+            }
+        }
+    }
+
+    let oddCount = 0
+
+    for (let key in obj) {
+        if (obj[key]%2 !== 0) {
+            oddCount++
+        }
+    }
+
+    if (oddCount > 1) return false
+    return true
 };
 
 // TESTS
 console.log(palinPerm('Tact Coa'), 'true');
 console.log(palinPerm('Tact boa'), 'false');
+console.log(palinPerm('race car'), 'true')
+console.log(palinPerm('racet car'), 'false')

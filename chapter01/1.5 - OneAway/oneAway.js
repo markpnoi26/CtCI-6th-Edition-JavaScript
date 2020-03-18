1151aa232522e822efeeec7c3301cff25c5e1106
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-03-18 09:03:23 -0700

    added working solution to One Away

--- chapter01/1.5 - OneAway/oneAway.js
+++ chapter01/1.5 - OneAway/oneAway.js
@@ -1,6 +1,30 @@
 var oneAway = function(string1, string2) {
+    let obj = {}
+    for (let i=0; i<string1.length; i++) {
+        if (obj[string1[i]]) {
+            obj[string1[i]]++
+        } else {
+            obj[string1[i]] = 1
+        }
+    }
 
-  
+    for (let i=0; i<string2.length; i++) {
+        if (obj[string2[i]]) {
+            obj[string2[i]]--
+        } 
+    }
+
+    let charCount = 0
+
+    for (let key in obj) {
+        if (obj[key] !== 0) {
+            charCount = charCount + obj[key]
+        }
+    }
+
+    if (charCount > 1) return false
+    return true
+
 };
 
 

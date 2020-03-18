467aacdc8d17c04064a9359f045ce4cfbf972679
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-03-18 09:04:00 -0700

    added working solution to String Compression

--- chapter01/1.6 - String Compression/strComp.js
+++ chapter01/1.6 - String Compression/strComp.js
@@ -1,5 +1,22 @@
 var strComp = function(string) {
-  
+
+    let curLetter = string[0]
+    let curCount = 0
+    let strAr = []
+
+    for (let i =0; i<string.length; i ++) {
+        if (string[i] === curLetter) {
+            curLetter = string[i]
+            curCount++
+        } else if (string[i] !== curLetter){
+            strAr.push(`${curLetter}${curCount}`)
+            curCount = 1
+            curLetter = string[i]
+        }
+    }
+    strAr.push(`${curLetter}${curCount}`)
+
+    return strAr.join("")
 };
 
 // Test

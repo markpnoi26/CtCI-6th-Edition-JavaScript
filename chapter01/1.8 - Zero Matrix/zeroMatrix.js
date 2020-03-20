30b00b41a06837f600f0917eb11083d4de91ebf8
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-03-19 19:18:34 -0700

    added working soltion to zeroMatrix

--- chapter01/1.8 - Zero Matrix/zeroMatrix.js
+++ chapter01/1.8 - Zero Matrix/zeroMatrix.js
@@ -1,8 +1,49 @@
 /* Helper Functions */
-var checkZeros = function(matrix) {
-  
+var zeroMatrix = function(matrix) {
+  // loop over array of arrays until you find a zero, then traverse up down left right converting all to zeros
+  // do it recursively
 
-};
+  for (let i=0; i<matrix.length;i ++) {
+    for (let j=0; j<matrix[i].length; j++) {
+      if (matrix[i][j] === 0) {
+        return setZero(matrix, i, j)
+      }
+    }
+  }
+}
+
+var setZero = function(matrix, row, col) {
+  
+  let upRow = row
+  let downRow = row
+  while (true) {
+    matrix[upRow][col] = 0
+    upRow--
+    if (upRow < 0) break
+  }
+
+  while (downRow !== matrix.length) {
+    matrix[downRow][col] = 0
+    downRow++
+  }
+
+  let upCol = col
+  let downCol = col
+
+  while (true) {
+    matrix[row][upCol] = 0
+    upCol--
+    if (upCol < 0) break
+  }
+
+  while (downCol !== matrix[row].length) {
+    matrix[row][downCol] = 0 
+    downCol++
+  }
+
+  return matrix
+
+}
 
 // Testing
 var testMatrix = [
@@ -13,6 +54,12 @@ var testMatrix = [
   [1, 1, 1, 1],
   [1, 1, 1, 1]
 ];
+
+function printMatrix(mtx) {
+  for (let el of mtx) {
+    console.log(el)
+  }
+}
 
 console.log('before');
 printMatrix(testMatrix);
